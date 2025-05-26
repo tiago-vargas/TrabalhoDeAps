6aa4466e03fe9761ad22dd1916cf8704e6706e25
Tiago Vargas <tiago.vargaspo@proton.me>
2025-05-26 01:20:49 -0300

    add Room and KSP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	alias(libs.plugins.android.application)
 	alias(libs.plugins.kotlin.android)
 	alias(libs.plugins.kotlin.compose)
-//	alias(libs.plugins.ksp)
+	alias(libs.plugins.ksp)
 }
 
 android {
@@ -68,7 +68,7 @@ dependencies {
 	debugImplementation(libs.androidx.ui.test.manifest)
 	implementation(libs.auth0.auth0)
 	implementation(libs.auth0.jwtdecode)
-//	ksp(libs.androidx.room.compiler)
+	ksp(libs.androidx.room.compiler)
 	implementation(libs.androidx.room.ktx)
 	implementation(libs.androidx.room.runtime)
 }
