a4e068fa105369a039e3b46444c28e69c354a952
Tiago Vargas <tiago.vargaspo@proton.me>
2025-06-24 17:18:51 -0300

    move login logic to `LoginScreen`
    breaks the preview

--- app/src/main/java/io/github/tiago_vargas/trabalhodeaps/LoginScreen.kt
+++ app/src/main/java/io/github/tiago_vargas/trabalhodeaps/LoginScreen.kt
@@ -1,42 +1,83 @@
 package io.github.tiago_vargas.trabalhodeaps
 
+import android.content.Context
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.callback.Callback
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.result.Credentials
 import io.github.tiago_vargas.trabalhodeaps.ui.theme.TrabalhoDeApsTheme
+import kotlinx.coroutines.launch
 
 @Composable
-fun LoginScreen(onLoginClicked: () -> Unit, modifier: Modifier = Modifier) {
+fun LoginScreen(
+	context: Context,
+	snackbarHostState: SnackbarHostState,
+	viewModel: LoginViewModel,
+	modifier: Modifier = Modifier,
+) {
+	val coroutineScope = rememberCoroutineScope()
+	val account = {
+		Auth0.getInstance(
+			clientId = context.getString(R.string.com_auth0_client_id),
+			domain = context.getString(R.string.com_auth0_domain),
+		)
+	}()
+	val attemptLogin = {
+		val callback = object : Callback<Credentials, AuthenticationException> {
+			override fun onFailure(error: AuthenticationException) {
+				coroutineScope.launch {
+					snackbarHostState.showSnackbar(
+						message = context.getString(R.string.an_error_occurred),
+					)
+				}
+			}
+
+			override fun onSuccess(result: Credentials) {
+				viewModel.setIsLoggedIn(true)
+			}
+		}
+		WebAuthProvider
+			.login(account)
+			.withScheme(context.getString(R.string.com_auth0_scheme))
+			.start(context, callback)
+	}
+
 	Column(
 		modifier = modifier,
 		verticalArrangement = Arrangement.Center,
 		horizontalAlignment = Alignment.CenterHorizontally,
 	) {
-		Button(onClick = onLoginClicked) {
+		Button(onClick = attemptLogin) {
 			Text("Login")
 		}
 	}
 }
 
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun LoginScreenPreview() {
-	TrabalhoDeApsTheme {
-		Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-			LoginScreen(
-				onLoginClicked = {},
-				modifier = Modifier
-					.fillMaxSize()
-					.padding(innerPadding),
-			)
-		}
-	}
-}
+//@Preview(showBackground = true, showSystemUi = true)
+//@Composable
+//fun LoginScreenPreview() {
+//	TrabalhoDeApsTheme {
+//		Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//			LoginScreen(
+//
+//				modifier = Modifier
+//					.fillMaxSize()
+//					.padding(innerPadding),
+//			)
+//		}
+//	}
+//}
--- app/src/main/java/io/github/tiago_vargas/trabalhodeaps/MainActivity.kt
+++ app/src/main/java/io/github/tiago_vargas/trabalhodeaps/MainActivity.kt
@@ -13,16 +13,9 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.auth0.android.Auth0
-import com.auth0.android.authentication.AuthenticationException
-import com.auth0.android.callback.Callback
-import com.auth0.android.provider.WebAuthProvider
-import com.auth0.android.result.Credentials
 import io.github.tiago_vargas.trabalhodeaps.ui.theme.TrabalhoDeApsTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,7 +29,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App(context: Context) {
-	val viewModel = viewModel<LoginViewModel>()
 	val snackbarHostState = remember { SnackbarHostState() }
 
 	TrabalhoDeApsTheme {
@@ -46,7 +38,6 @@ fun App(context: Context) {
 		) { innerPadding ->
 			Content(
 				context = context,
-				viewModel = viewModel,
 				modifier = Modifier
 					.fillMaxSize()
 					.padding(innerPadding),
@@ -59,43 +50,14 @@ fun App(context: Context) {
 @Composable
 fun Content(
 	context: Context,
-	viewModel: LoginViewModel,
-	modifier: Modifier = Modifier,
 	snackbarHostState: SnackbarHostState,
+	modifier: Modifier = Modifier,
 ) {
-//	val isLoggedIn = viewModel.isLoggedIn.collectAsState()
-//	val coroutineScope = rememberCoroutineScope()
-//
-//	val account = Auth0.getInstance(
-//		clientId = context.getString(R.string.com_auth0_client_id),
-//		domain = context.getString(R.string.com_auth0_domain),
-//	)
-//	val attemptLogin = {
-//		val callback = object : Callback<Credentials, AuthenticationException> {
-//			override fun onFailure(error: AuthenticationException) {
-//				coroutineScope.launch {
-//					snackbarHostState.showSnackbar(
-//						message = context.getString(R.string.an_error_occurred),
-//					)
-//				}
-//			}
-//
-//			override fun onSuccess(result: Credentials) {
-//				viewModel.setIsLoggedIn(true)
-//			}
-//		}
-//		WebAuthProvider
-//			.login(account)
-//			.withScheme(context.getString(R.string.com_auth0_scheme))
-//			.start(context, callback)
-//	}
-
-//	if (isLoggedIn.value) {
-//		PetListScreen(onPetClicked = { pet -> }, onAddClicked = {}, modifier = modifier)
-//		LogoutScreen(onLogoutClicked = attemptLogout, modifier = modifier)
-//		PetListScreen(onPetClicked = { pet -> }, onAddClicked = {}, modifier = modifier)
+	val viewModel = viewModel<LoginViewModel>()
+	val isLoggedIn = viewModel.isLoggedIn.collectAsState()
+	if (isLoggedIn.value) {
 		PrePetListScreen(modifier = modifier)
-//	} else {
-//		LoginScreen(onLoginClicked = attemptLogin, modifier = modifier)
-//	}
+	} else {
+		LoginScreen(context = context, viewModel = viewModel, snackbarHostState = snackbarHostState, modifier = modifier)
+	}
 }
