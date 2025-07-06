19331fe227cb79110801fa4f87a848c332ff5d14
Tiago Vargas <tiago.vargaspo@proton.me>
2025-07-06 12:34:57 -0300

    add `generativeai` module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+	implementation(libs.generativeai)
 	implementation(libs.androidx.core.ktx)
 	implementation(libs.androidx.lifecycle.runtime.ktx)
 	implementation(libs.androidx.lifecycle.viewmodel.compose)
